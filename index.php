1dd135493227073066e4a0ceb33f9b6c809f2885
andycrr <AndyCooperX1@gmail.com>
2020-04-24 16:04:54 +0300

    added function curl_get for curl operations updated simple_html_dom

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 
+include_once __DIR__ .'/lib/curl_query.php';
 include_once __DIR__ .'/lib/curl.php';
 include_once __DIR__ .'/lib/simple_html_dom.php';
 
-$html = file_get_contents('https://ntschool.ru/kursyi');
+//$html = file_get_contents('https://ntschool.ru/kursyi');
+
+$html = curl_get('https://ntschool.ru/kursyi');
+$dom = str_get_html($html);
+
+$courses = $dom->find('.courses-list--item-body--title');
+//var_dump($courses);
+
+foreach ($courses as $course) {
+//    var_dump(3);
+    echo $course->plaintext . '<br>';
+}
 
